0f1972a9003e2249f7aa6123ab280c5634af5895
rm076440 <143750062+rm076440@users.noreply.github.com>
2023-11-16 14:43:20 -0700

    Add files via upload

---
+++ Project7/start/myscripts.js
@@ -0,0 +1,25 @@
+<!doctype html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>Untitled Document</title>
+</head>
+
+<body onload="preloadImages()">
+    <div>
+        <a href="" onmouseover="document.getElementById('print').src='Images/Print2.png';"
+            onmouseout="document.getElementById('print').src='Images/Print1.png';">
+
+            <img src="Images/Print1.png" alt="Print Page" width="200px" height="50px" id="print">
+
+        </a>
+    </div>
+    <div>
+        <a href="" onmouseover="document.getElementById('logo').src='Images/Logo2.png';"
+            onmouseout="document.getElementById('logo').src='Images/Logo.png';">
+
+            <img src="Images/Logo.png" alt="Logo" width="150px" height="175px" id="logo">
+
+        </a>
+    </div>
+</html>
